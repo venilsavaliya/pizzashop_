e73445051d03100993c7124c35dcaed56936bf70
venil <venilsavaliya999@gmail.com>
2025-03-11 21:13:25 +0530

    changing versions

--- DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990cf7643f06ff415ed49f55ead9aa401256364e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3955531f217435ebdecad4c466e518948e0526")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
